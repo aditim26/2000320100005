14f46db1e819bbb90be8be43a7edfdf9b23e03ef
Aditi Mittal <aditim26@yahoo.com>
2023-07-22 17:27:49 +0530

    components files added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Alltrain from "./components/Alltrain";
+import Navbar from "./components/Navbar";
+import Detail from "./components/Detail";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Navbar/>
+    <Routes>
+      <Route path="/" element={<Alltrain/>}/>
+      <Route path="/trains" element={<Alltrain/>} />
+      <Route path="/detail/:number" element={<Detail/>} />
+    </Routes>
+    </BrowserRouter>
   );
 }
 
